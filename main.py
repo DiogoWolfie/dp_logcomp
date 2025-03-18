d1a1b8f822a39ca1ac018dc6fae48a5d6e82bc07
DiogoWolfie <diogopl1@al.insper.edu.br>
2025-03-18 16:09:25 -0300

    5

--- main.py
+++ main.py
@@ -215,7 +215,7 @@ class NoPrt(Node):
     def __init__(self, children):
         super().__init__(None,children)
     def Evaluate(self, SymbolTable):
-        return print(self.children.Evaluate(SymbolTable))
+        return print(int(self.children.Evaluate(SymbolTable)))
 
 #NoBlc- nó do bloco
 class NoBlc(Node):
