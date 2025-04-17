38a38d1362c81c1cdeabcd6f9999575a5be1f9ea
DiogoWolfie <diogopl1@al.insper.edu.br>
2025-04-17 14:18:55 -0300

    tentativa 6

--- ast/nodes/readnode.py
+++ ast/nodes/readnode.py
@@ -1,16 +1,3 @@
-# from ast.node import Node
-# #preciso construir um nó par ao read em go	
-
-# class ReadNode(Node):
-#     def __init__(self, variable):
-#         super().__init__("read", [variable])
-#         self.variable = variable
-
-#     def Evaluate(self, SymbolTable):
-#         # Read a value from the input and store it in the variable
-#         value = input(f"Enter value for {self.variable.name}: ")
-#         SymbolTable.SetValue(self.variable.name, value)
-#         return None
 from ast.node import Node
 
 class ReadNode(Node):
@@ -19,4 +6,4 @@ class ReadNode(Node):
     
     def Evaluate(self, st):
         # Lê do terminal e converte para inteiro
-        return int(input(">"))
+        return int(input())
